fbb833906dfa4b71cf488fe0b613c2bce227592f
LH03 <lea.helbok03@htl-salzburg.ac.at>
2021-06-02 20:05:00 +0200

    add scripts folder and finish assignment task 4

--- array-LH03/Assets/Scripts/Arrays.cs
+++ array-LH03/Assets/Scripts/Arrays.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Arrays : MonoBehaviour
 {
@@ -10,26 +11,33 @@ public class Arrays : MonoBehaviour
     float[] array2 = new float[4] { 1f, 2f, 3f, 4f };
     int[] array3 = { 1,2,3,4};
 
+    //Aufgaben:
     //1) Durchlaufen ausgeben der Werte
     //2) Länge des Arrays ausgeben als Debug
     //3) 2 Werte direkt ändern
     //4) Gameobjects Images anlegen, Tag vergeben, Zuweisen und Farbe ändern
 
-    //30min Projekt + Arrays Aufgabe -> Issues reinschreiben! Github pushen! 
-
 
     void Start()
     {
+        //Initialwerte der Arrays ausgeben
         OutputValuesArrays();
+
+        //Länge der Arrays in Konsole ausgeben
         Debug.Log("length array 1: " + array1.Length);
         Debug.Log("length array 2: " + array2.Length);
         Debug.Log("length array 3: " + array3.Length);
 
+        //Werte von Arrays direkt modifizieren
         array2[2] = 2.6f;
         array1[3] = "fünf";
 
+        //Geänderte Arrays ausgeben
         OutputValuesArrays();
 
+        //Farbe von Bild ändern
+        GameObject[] arrayImages = GameObject.FindGameObjectsWithTag("RedImage");
+        arrayImages[2].GetComponent<Image>().color = Color.red;
     }
 
     void OutputValuesArrays()
@@ -42,9 +50,5 @@ public class Arrays : MonoBehaviour
         }
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+
 }
